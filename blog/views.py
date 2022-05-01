e3f43ac00ff72557b8a841598591ebb4ab0028ed
Kye <kyelindholm@gmail.com>
2022-05-01 00:32:35 -0400

    basic funcionalities of the django blog

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from typing import List
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post
+
+class PostListView(ListView):
+  queryset = Post.published.all()
+  context_object_name = 'posts'
+  paginate_by = 3
+  template_name = 'blog/post/list.html'
+
+def post_detail(request, year, month, day, post):
+  post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
+
+  return render(request, 'blog/post/detail.html', {'post': post})
