6d9e6f6b443b3c783737e1df01ab47d6b673e110
Kye <kyelindholm@gmail.com>
2022-05-01 12:00:02 -0400

    implemented email-sending backend for recommending posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
+from re import sub
 from typing import List
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
+
 from .models import Post
+from .forms import EmailPostForm
 
 class PostListView(ListView):
   queryset = Post.published.all()
@@ -12,5 +15,23 @@ class PostListView(ListView):
 
 def post_detail(request, year, month, day, post):
   post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-
   return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+  #get post by id
+  post = get_object_or_404(Post, id=post_id, status='published')
+  sent = False
+
+  if request.method == 'POST':
+    form = EmailPostForm(request.POST)
+    if form.is_valid():
+      cd = form.cleaned_data
+      post_url = request.build_absolute_uri(post.get_absolute_url())
+      subject = f"{cd['name']} recommends you read " f"{post.title}"
+      message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+      send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+      sent = True
+  else:
+    form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
